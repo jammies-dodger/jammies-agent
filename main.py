acdfd622cd5795d7bd4b6deb91335c2acd701c15
jammies <strangecomputerbandit@gmail.com>
2026-09-17 12:18:34 +0100

    refactor: restructure main.py to improve prompt handling and argument parsing

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
+parser = argparse.ArgumentParser(description="jammies ai agent chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+## load env vars
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
+prompt_mode = str.lower(os.environ.get("PROMPT_MODE"))
 
-if api_key is None: raise RuntimeError('OPENROUTER_API_KEY could not be loaded from .env file')
+def runPrompt():
+    if prompt_mode == "disable" or prompt_mode == "disabled": 
+        print(f"---prompt skipped---")
+        return 
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key
-)
+    if api_key is None: raise RuntimeError('OPENROUTER_API_KEY could not be loaded from .env file')
 
-response = client.chat.completions.create(
-    model="openrouter/free",
-    messages= [
-        {
-            "role": "user",
-            "content": "Write a very short open ended story max 3 sentences."
-        }
-    ]
-)
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key
+    )
 
-if response.usage is None: raise RuntimeError('response.usage is None, api request likely failed')
+    user_prompt = args.user_prompt
 
-# Track token usage
-prompt_tokens = response.usage.prompt_tokens
-completion_tokens = response.usage.completion_tokens
-print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {completion_tokens}")
+    if user_prompt is None or len(user_prompt) == 0: raise TypeError('Missing prompt')
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages= [
+            {
+                "role": "user",
+                "content": user_prompt
+            }
+        ]
+    )
+
+    if response.usage is None: raise RuntimeError('response.usage is None, api request likely failed')
+
+    # Track token usage
+    prompt_tokens = response.usage.prompt_tokens
+    completion_tokens = response.usage.completion_tokens
+    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {completion_tokens}")
 
 
-print(response.choices[0].message.content)
+    print(response.choices[0].message.content)
+
+
+runPrompt()
