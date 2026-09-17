85aed5b1047ef3138c6ade7f327a05a745a0609f
jammies <strangecomputerbandit@gmail.com>
2026-09-17 23:27:13 +0100

    guardrails

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,9 +1,9 @@
 from calculator.functions.get_files_info import get_files_info
 test_results = [
     get_files_info("calculator", "."),
+    get_files_info("calculator", "pkg"),
     get_files_info("calculator", "/bin"),
-    get_files_info("calculator", "../"),
-    get_files_info("calculator", "main.py")
+    get_files_info("calculator", "../")
 ]
 
 for result in test_results: print(result)
