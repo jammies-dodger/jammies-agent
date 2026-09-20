422e3ce39037edfd3c86018e397886fe72db2f58
jammies <strangecomputerbandit@gmail.com>
2026-09-20 21:02:11 +0100

    feat: add system prompt to enhance chatbot response handling

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
+from prompts import system_prompt
+
 parser = argparse.ArgumentParser(description="jammies ai agent chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
@@ -14,9 +16,9 @@ api_key = os.environ.get("OPENROUTER_API_KEY")
 prompt_mode = str.lower(os.environ.get("PROMPT_MODE"))
 
 def generate_content():
-    if prompt_mode == "disable" or prompt_mode == "disabled": 
+    if prompt_mode == "disable" or prompt_mode == "disabled":
         print(f"---prompt skipped---")
-        return 
+        return
 
     if api_key is None: raise RuntimeError('OPENROUTER_API_KEY could not be loaded from .env file')
 
@@ -31,6 +33,10 @@ def generate_content():
 
     messages = [
         {
+            "role": "system",
+            "content": system_prompt
+        },
+        {
             "role": "user",
             "content": user_prompt
         }
@@ -39,21 +45,24 @@ def generate_content():
 
     response = client.chat.completions.create(
         model="openrouter/free",
-        messages=messages
+        messages=messages,
+        temperature=0
     )
+
+
 
     if response.usage is None: raise RuntimeError('response.usage is None, api request likely failed')
 
     # Track token usage
-    
-    if args.verbose: 
+
+    if args.verbose:
         prompt_tokens = response.usage.prompt_tokens
         completion_tokens = response.usage.completion_tokens
-
+        model = response.model
+        print(f"Model: {model}")
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {completion_tokens}")
 
-    
     print(response.choices[0].message.content)
 
 generate_content()
